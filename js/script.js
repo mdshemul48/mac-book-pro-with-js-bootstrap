4c1eb6ae6ea1628b120b49e7e2e8213fc7271977
MD Shimul <mdshemul480@gmail.com>
2021-08-21 00:48:31 +0600

    adding delivery option

--- js/script.js
+++ js/script.js
@@ -22,3 +22,7 @@ setListenerAndUpdatePrice("memory-16gb-option", "extra-memory-price", 180);
 setListenerAndUpdatePrice("storage-option-256gb", "extra-storage-price", 0);
 setListenerAndUpdatePrice("storage-option-512gb", "extra-storage-price", 30);
 setListenerAndUpdatePrice("storage-option-1tb", "extra-storage-price", 50);
+
+//  ===================== delivery price updating =================================
+setListenerAndUpdatePrice("delivery-free-option", "delivery-charge-price", 0);
+setListenerAndUpdatePrice("delivery-paid-option", "delivery-charge-price", 20);
